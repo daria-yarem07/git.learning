d55533cfe744ad50c63cda63e3ba535194a930c7
Daria Yaremchuk <daria.yaremchuk777@gmail.com>
2021-07-31 14:21:53 +0300

    git.d_yaremchuk

---
+++ module.py
@@ -0,0 +1,9 @@
+import random
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+x = random.sample(list, 1)
+
+mydict = {
+    "brand:": "Dodge",
+    "model:": "Charger",
+    "year:": 2020
+}
---
+++ module_unit_14.py
@@ -0,0 +1,5 @@
+from module import mydict
+print(mydict["model:"])
+
+from module import x
+print(x)
